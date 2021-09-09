442fa40f41c23f523a542408ca003bc1d6917f95
thatielys <thatielys@DESKTOP-17GLA63.localdomain>
2021-09-09 19:28:52 -0300

    aula 5

--- controller/produto_controller.js
+++ controller/produto_controller.js
@@ -1,47 +1,48 @@
 const produtos = [
-  {id:1, nome:"produto1", preco: 10},
-  {id:2, nome:"produto2", preco: 20},
-  {id:3, nome:"produto3", preco: 35}
+  {id:1, nome:"produto1", preco: 10 },
+  {id:2, nome:"produto2", preco: 20 },
+  {id:3, nome:"produto3", preco: 30 }
 ];
 
 let idGerado = 4;
 
 exports.listar = (req, res) => {
-    res.json(produtos);
+  res.json(produtos)
 }
 
-exports.buscarPorId =  (req, res) => {
-    for (const produto of produtos){
-        if(req.params.id == produto.id){
-            res.json(produto);
-        }
-    }
-    res.status(404).json({erro:"Produto não encontrado"});
+exports.buscarPorId = (req, res) => {
+  for (const produto of produtos) {
+    if(req.params.id == produto.id) {
+      return res.json(produto);
+    }      
+  }
+  res.status(404).json({erro:"Produto nao encontrado"});
 }
 
-exports.novoProduto = (req, res) => {
-    const novoProduto = req.body;
-    novoProduto.id = idGerado++;
-    produtos.push(novoProduto);
-    res.status(201).json(novoProduto);
+exports.inserir = (req, res) => {
+  const novoProduto = req.body;
+  novoProduto.id = idGerado++;
+  produtos.push(novoProduto);
+  return res.status(201).json(novoProduto);
 }
 
-exports.editarProduto = (req, res) => {
-    for (const produto of produtos){
-      if(req.params.id == produto.id){
-        produto.nome = req.body.nome;
-        produto.preco = req.body.preco;
-        res.json(produto);
-      }
-    }
-    res.status(404).json({erro:"Produto não encontrado"});
+exports.atualizar = (req, res) => {
+  for (const produto of produtos) {
+    if(req.params.id == produto.id) {
+      produto.nome = req.body.nome;
+      produto.preco = req.body.preco;
+      return res.json(produto);
+    }      
+  }
+  res.status(404).json({erro:"Produto nao encontrado"});
 }
 
-exports.deletarProduto = (req,res) => {
-    for (const indice in produtos){
-      if(req.params.id == produtos[indice].id){
-        const produtoDeletado = produtos.splice(indice,1);
-        res.json(produtoDeletado[0]);
-      }
-    }
+exports.deletar  = (req, res) => {
+  for (const indice in produtos) {
+    if(req.params.id == produtos[indice].id) {
+      const produtoDeletado = produtos.splice(indice,1);
+      return res.json(produtoDeletado[0]);
+    }      
+  }
+  res.status(404).json({erro:"Produto nao encontrado"});
 }
